303d8b69760db355f604398059d0a8a87e15d363
杨虎 <yanghu@kuainiugroup.com>
2017-12-06 10:17:43 +0800

    no message

--- environments/dev/backend/config/main-local.php
+++ environments/dev/backend/config/main-local.php
@@ -11,14 +11,28 @@ $config = [
 
 if (!YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
+    $config['bootstrap'][]      = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
     ];
 
-    $config['bootstrap'][] = 'gii';
+    $config['bootstrap'][]    = 'gii';
     $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
+        'class'      => 'yii\gii\Module',
+        'generators' => [
+            'crud'  => [
+                'class'     => 'yh\gii\generators\crud\Generator',
+                'templates' => [
+                    'zh' => '@common/yh-gii/generators/crud/default',
+                ]
+            ],
+            'model' => [
+                'class'     => 'yii\gii\generators\model\Generator',
+                'templates' => [
+                    'zh' => '@common/yh-gii/generators/model/default',
+                ]
+            ],
+        ]
     ];
 }
 
